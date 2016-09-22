09958e16e7c79fc3edd805d88629d520a59fad1e
Andreas Kloeckner <inform@tiker.net>
2016-09-22 01:14:46 -0500

    Remove old django settings import

--- expensely/settings.py
+++ expensely/settings.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 from os.path import join
 
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
